21a45fccab419adf1c41e316cdc8cc2127a710fc
Rijan Majhi <rijan500mj@gmail.com>
2024-02-26 23:23:52 +0545

    Initial commit

---
+++ database/migrations/2024_01_31_010345_create_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('cus_id');
+            $table->string('tour_id');
+            $table->string('duration');
+            $table->string('booking_by');
+            $table->decimal('amount', 10, 2);
+            $table->decimal('dis_amount', 10, 2);
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
